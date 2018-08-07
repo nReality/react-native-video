c7ff9096222519185eb935381733a908366ee018
Jacques de Vos <jacdevos@gmail.com>
2018-08-07 13:41:20 +0200

    Close fullscreen when playback ends to avoid hanging bug

--- ios/RCTVideo.m
+++ ios/RCTVideo.m
@@ -100,6 +100,9 @@ static NSString *const timedMetadata = @"timedMetadata";
     playerLayer.view.frame = self.bounds;
     playerLayer.player = player;
     playerLayer.view.frame = self.bounds;
+  
+    //ensure the full screen ends to avoid crash when trying to close on end from react native
+    playerLayer.exitsFullScreenWhenPlaybackEnds = YES;
     return playerLayer;
 }
 
